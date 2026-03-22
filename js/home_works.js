eb6b7fbb8d37da429973d1b65176e5b84310b46e
Elzar <elzar.eduard@gmail.com>
2026-03-22 17:28:32 +0600

    homework_lesson5

---
+++ js/home_works.js
@@ -0,0 +1,100 @@
+const gmailInput = document.querySelector("#gmail_input");
+const gmailButton = document.querySelector("#gmail_button");
+const gmailResult = document.querySelector("#gmail_result");
+
+const regExp = /^[a-zA-Z0-9][a-zA-Z0-9._]{4,28}[a-zA-Z0-9]@gmail\.com$/;
+
+gmailButton.addEventListener("click", () => {
+  if (regExp.test(gmailInput.value)) {
+    gmailResult.innerHTML = "OK";
+    gmailResult.style.color = "green";
+  } else {
+    gmailResult.innerHTML = "not OK";
+    gmailResult.style.color = "red";
+  }
+});
+
+const childBlock = document.querySelector(".child_block");
+let positionX = 0;
+let positionY = 0
+const maxPosition = 449;
+
+const moveBlock = () => {
+  if (positionX < maxPosition && positionY === 0) {
+    positionX++;
+    // childBlock.style.left = `${positionX}px`;
+    // requestAnimationFrame(moveBlock);
+  } else if (positionX >= maxPosition && positionY < maxPosition) {
+    positionY++;
+  } else if (positionX > 0 && positionY >= maxPosition) {
+    positionX--;
+  } else if (positionX === 0 && positionY > 0) {
+    positionY--;
+  }
+  childBlock.style.left = `${positionX}px`;
+  childBlock.style.top = `${positionY}px`;
+  requestAnimationFrame(moveBlock);
+};
+moveBlock();
+
+// основы асинхроности
+// синхронный код
+// console.log (1)
+// console.log (2)
+// console.log (3)
+
+const secondsBlock = document.querySelector("#seconds");
+const startBtn = document.querySelector("#start");
+const stopBtn = document.querySelector("#stop");
+const resetBtn = document.querySelector("#reset");
+
+let count = 0;
+
+let intervalId = null;
+
+startBtn.addEventListener("click", () => {
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      count++;
+      secondsBlock.innerHTML = count;
+    }, 1000);
+  }
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(intervalId);
+
+  intervalId = null;
+});
+
+resetBtn.addEventListener("click", () => {
+  clearInterval(intervalId);
+  intervalId = null;
+
+  count = 0;
+
+  secondsBlock.innerHTML = count;
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const charactersContainer = document.getElementById("characters-container");
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "../data/any.json");
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      const characters = JSON.parse(xhr.response);
+      characters.forEach((character) => {
+        const card = document.createElement("div");
+        card.classList.add("character-card");
+        card.innerHTML = `<img src="${character.photoUrl}" alt="${character.name}"><h2>${character.name}</h2>
+            <div class="role">${character.role}</div>
+            <p>${character.description}</p>`;
+        charactersContainer.appendChild(card);
+      });
+    } else {
+      console.error("Error");
+      charactersContainer.innerHTML = "<p>не удалось загрузить данные </p>";
+    }
+  };
+  xhr.send();
+});
---
+++ js/lesson.js
@@ -0,0 +1,127 @@
+// Phone NUMBER VALIDATE
+
+// const phoneInput = document.querySelector('#phone')
+// tab slider
+const tabContentBlocks = document.querySelectorAll(".tab_content_block");
+const tabContentItems = document.querySelectorAll(".tab_content_item");
+const tabContentItemsParent = document.querySelector(".tab_content_items");
+
+const hideTabContent = () => {
+  tabContentBlocks.forEach((tabBlock) => {
+    tabBlock.style.display = "none";
+  });
+  tabContentItems.forEach((tabItem) => {
+    tabItem.classList.remove("tab_content_item_active");
+  });
+};
+const showTabContent = (i = 0) => {
+  tabContentBlocks[i].style.display = "block";
+  tabContentItems[i].classList.add("tab_content_item_active");
+};
+hideTabContent();
+showTabContent();
+
+tabContentItemsParent.onclick = (event) => {
+  if (event.target.classList.contains("tab_content_item")) {
+    tabContentItems.forEach((tabItem, tabIndex) => {
+      if (event.target === tabItem) {
+        hideTabContent();
+        showTabContent(tabIndex);
+        currentSlideIndex = tabIndex;
+      }
+    });
+  }
+};
+
+let currentSlideIndex = 0;
+const autoSlider = () => {
+  currentSlideIndex++;
+  if (currentSlideIndex >= tabContentBlocks.length) {
+    currentSlideIndex = 0;
+  }
+  hideTabContent();
+  showTabContent(currentSlideIndex);
+};
+setInterval(autoSlider, 3000);
+
+const cardBlock = document.querySelector(".card");
+const btnNext = document.querySelector("#btn-next");
+const btnPrev = document.querySelector("#btn-prev");
+btnNext.onclick = () => {
+  fetch("https://jsonplaceholder.typicode.com/todos/201")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+};
+
+const searchInput = document.querySelector("#searchInput");
+const searchButton = document.querySelector("#search");
+const city = document.querySelector(".city");
+const temp = document.querySelector(".temp");
+
+// API key 291aa3950880603684e43c6cc36aed88
+//query params = параметры запроса
+const VERSION = "2.5";
+const API_KEY = "291aa3950880603684e43c6cc36aed88";
+const BASE_API = "https://api.openweathermap.org/data/${VERSION}/weather";
+
+searchButton.onclick = () => {
+  if (searchInput.value !== "") {
+    fetch(
+      "${BASE_API}?q=${searchInput.value}&units=metric&lang=ru&appid=${API_KEY}",
+    )
+      .then((response) => response.json()) // promise <any>
+      .then((data) => {
+        console.log(data.name, data.main.temp);
+        city.innerHTML = data.name;
+        temp.innerHTML = Math.round(data.main.temp) + "&deg;C";
+      });
+    searchInput.value = "";
+  } else {
+    city.innerHTML = "Введите название города";
+    temp.innerHTML = "";
+    city.style.color = "red";
+  }
+};
+//домашнее задание урока № 5
+const somInput = document.querySelector("#som");
+const usdInput = document.querySelector("#usd");
+const eurInput = document.querySelector("#eur");
+
+const converter = (element, targetElement1, targetElement2) => {
+  element.oninput = () => {
+    const request = new XMLHttpRequest();
+    request.open("GET", "../data/converter.json");
+    request.setRequestHeader("Content-type", "application/json");
+    request.send();
+    request.onload = () => {
+      const data = JSON.parse(request.response);
+      if (element.value === "") {
+        targetElement1.value = "";
+        targetElement2.value = "";
+        return;
+      }
+      if (element.id === "som") {
+        targetElement1.value = (element.value / data.usd).toFixed(2);
+        targetElement2.value = (element.value / data.eur).toFixed(2);
+      }
+      if (element.id === "usd") {
+        targetElement1.value = (element.value * data.usd).toFixed(2);
+        targetElement2.value = ((element.value * data.usd) / data.eur).toFixed(
+          2,
+        );
+      }
+
+      if (element.id === "eur") {
+        targetElement1.value = (element.value * data.eur).toFixed(2);
+        targetElement2.value = ((element.value * data.eur) / data.usd).toFixed(
+          2,
+        );
+      }
+    };
+  };
+};
+converter(somInput, usdInput, eurInput);
+converter(usdInput, somInput, eurInput);
+converter(eurInput, somInput, usdInput);
